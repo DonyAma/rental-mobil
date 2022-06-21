ad7e2759ac937d60589d590f808f188ab2e59ee2
Fawaz191001 <fawazbmc191001@gmail.com>
2022-06-21 09:14:48 +0700

    Menghapus penambahan Upload di Libraries

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('session', 'form_validation', 'database','upload');
+$autoload['libraries'] = array('session', 'form_validation', 'database',);
 
 /*
 | -------------------------------------------------------------------
