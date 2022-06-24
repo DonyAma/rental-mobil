92d225b21f93b896e4b18d09c503f08255bd0ad3
DonyAma <amadony18@gmail.com>
2022-06-24 20:31:27 +0700

    Membuat folder database di dalam folder assets untuk menyimpan file database yg sudah di expor

---
+++ assets/database/rental-mobil.sql
@@ -0,0 +1,229 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 18 Jun 2022 pada 10.23
+-- Versi server: 10.1.38-MariaDB
+-- Versi PHP: 5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rental-mobil`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `nama_admin` varchar(120) NOT NULL,
+  `username` varchar(120) NOT NULL,
+  `password` varchar(120) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `customer`
+--
+
+CREATE TABLE `customer` (
+  `id_customer` int(11) NOT NULL,
+  `nama` varchar(120) NOT NULL,
+  `username` varchar(120) NOT NULL,
+  `alamat` varchar(120) NOT NULL,
+  `gender` varchar(20) NOT NULL,
+  `no_telepon` varchar(20) NOT NULL,
+  `no_ktp` varchar(50) NOT NULL,
+  `password` varchar(120) NOT NULL,
+  `no_hash` varchar(120) NOT NULL,
+  `role_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mobil`
+--
+
+CREATE TABLE `mobil` (
+  `id_mobil` int(11) NOT NULL,
+  `kode_type` varchar(120) NOT NULL,
+  `merk` varchar(120) NOT NULL,
+  `no_plat` varchar(20) NOT NULL,
+  `warna` varchar(20) NOT NULL,
+  `tahun` varchar(4) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `denda` int(11) NOT NULL,
+  `supir` int(11) NOT NULL,
+  `transmisi` varchar(20) NOT NULL,
+  `tmpt_duduk` int(11) NOT NULL,
+  `gambar` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `mobil`
+--
+
+INSERT INTO `mobil` (`id_mobil`, `kode_type`, `merk`, `no_plat`, `warna`, `tahun`, `status`, `harga`, `denda`, `supir`, `transmisi`, `tmpt_duduk`, `gambar`) VALUES
+(4, 'MVP', 'Daihatsu Great New Xenia', 'B 9923 AFK', 'Putih', '2015', '0', 150000, 50000, 0, 'Otomatis', 6, 'Daihatsu-Great-New-Xenia.jpg'),
+(6, 'MVP', 'Honda Mobilio', 'B 9923 AFK', 'Putih', '2015', '1', 150000, 10000, 1, 'Manual', 7, 'car-rental-honda-mobilio1.jpg'),
+(9, 'MVP', 'honda', 'B 4534 ck', 'putih', '2013', '', 23, 122, 1, 'Manual', 4, 'car-rental-honda-mobilio14.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `rental`
+--
+
+CREATE TABLE `rental` (
+  `id_rental` int(11) NOT NULL,
+  `id_customer` int(11) NOT NULL,
+  `tanggal_rental` date NOT NULL,
+  `tanggal_kembali` date NOT NULL,
+  `tanggal_pengembalian` date NOT NULL,
+  `status_rental` varchar(50) NOT NULL,
+  `status_pengembalian` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_rental` int(11) NOT NULL,
+  `id_customer` int(11) NOT NULL,
+  `id_mobil` int(11) NOT NULL,
+  `tanggal_rental` date NOT NULL,
+  `tanggal_kembali` date NOT NULL,
+  `denda` varchar(120) NOT NULL,
+  `total_denda` varchar(120) NOT NULL,
+  `harga` varchar(120) NOT NULL,
+  `tanggal_pengembalian` date NOT NULL,
+  `status_pengembalian` varchar(50) NOT NULL,
+  `status_rental` varchar(50) NOT NULL,
+  `bukti_pembayaran` varchar(120) NOT NULL,
+  `status_pembayaran` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `type`
+--
+
+CREATE TABLE `type` (
+  `id_type` int(11) NOT NULL,
+  `kode_type` varchar(10) NOT NULL,
+  `nama_type` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `type`
+--
+
+INSERT INTO `type` (`id_type`, `kode_type`, `nama_type`) VALUES
+(2, 'MVP', 'Multi Purpose Vehicle'),
+(5, 'CC', 'City Car');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indeks untuk tabel `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id_customer`);
+
+--
+-- Indeks untuk tabel `mobil`
+--
+ALTER TABLE `mobil`
+  ADD PRIMARY KEY (`id_mobil`);
+
+--
+-- Indeks untuk tabel `rental`
+--
+ALTER TABLE `rental`
+  ADD PRIMARY KEY (`id_rental`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_rental`);
+
+--
+-- Indeks untuk tabel `type`
+--
+ALTER TABLE `type`
+  ADD PRIMARY KEY (`id_type`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id_customer` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `mobil`
+--
+ALTER TABLE `mobil`
+  MODIFY `id_mobil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT untuk tabel `rental`
+--
+ALTER TABLE `rental`
+  MODIFY `id_rental` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_rental` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `type`
+--
+ALTER TABLE `type`
+  MODIFY `id_type` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
